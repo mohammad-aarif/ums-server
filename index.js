f511bfbdb1d67331c94981f964511e6d6d7889be
mohammad-aarif <arifbd354@gmail.com>
2025-02-17 19:23:30 +0600

    MongoDb intialize

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const {MongoClient , ServerApiVersion} =require('mongodb');
 
 const port = 5000;
-const app = express()
+const app = express();
 
 
 // Middleware 
 app.use(cors())
 
+
+// Database connction 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fs6ek.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const mongo = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+})
+
+
+async function run() {
+    try{
+        await mongo.connect()
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
 
 // Home 
 app.get('/', (req, res) => {
@@ -20,5 +44,4 @@ app.get('/', (req, res) => {
 // Server Connection 
 app.listen(port, () => {
     console.log("Server Connected!");
-    
 })
