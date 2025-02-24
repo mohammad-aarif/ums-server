b4111310b51f290bbbf17ebf8f7c365168684ea2
mohammad-aarif <arifbd354@gmail.com>
2025-02-24 21:03:29 +0600

    jwt filter added and user count system added

--- index.js
+++ index.js
@@ -18,6 +18,22 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
+const tokenVerify = (req, res, next) => {
+    const token = req.cookies?.token
+    if(!token){
+        res.status(401).send({message: 'Unauthorized Access!'})
+    }
+    jwt.verify(token, process.env.JWT_SECRET, (err, decode) =>{
+        if(err){
+            res.status(401).send({message: "Unauthorized Access"})
+        }
+        else{
+            req.body = decode
+            next()
+        }
+    })
+}
+
 
 // Database connction 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fs6ek.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -61,6 +77,10 @@ async function run() {
             const result = await userCollection.find().toArray();
             res.send(JSON.stringify(result))
         })
+        app.get('/userscount', async(req, res) => {
+            const usersCount = await userCollection.estimatedDocumentCount()
+            res.send({usersCount})
+        })
         // Sending Data to the Database 
         app.post('/users', async(req, res) =>{
             const userData = req.body
@@ -68,6 +88,17 @@ async function run() {
             res.send(result);                       
         })
         // Deleting data from Database 
+        app.get('/users/view/:id', tokenVerify, async(req, res) => {
+            const id = req.params.id
+            const data = req.body?.email
+            if(data !== id) {
+                res.status(401).send({message: "Your Are Unauthorzed"})
+            }
+            else{
+            const query = {email: id}
+            const result = await userCollection.find(query).toArray()
+            res.send(result)}
+        })
         app.delete('/users/:id', async(req, res) => {
             const id = req.params.id
             const query = {_id: new ObjectId(id)}
